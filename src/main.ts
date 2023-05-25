965c144910696e7484350ac861de52718eda1d3a
Sergey <spushkovskiy@gmail.com>
2023-05-25 20:58:05 +0300

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const logger = new Logger(AppModule.name);
+  const PORT = process.env.PORT;
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('/api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
+
+  await app.listen(PORT);
+  logger.debug(`Server started on port ${PORT}`);
+}
+bootstrap();
